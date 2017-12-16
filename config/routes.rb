fc051c9b4f11357085e587345e5553ec8caf1f6d
Jonadelgado <jonathan.delgado25@gmail.com>
2017-12-16 03:40:54 -0300

    Subida de sistema para gestion de ventas y stock a Git

---
+++ config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  root to: 'dashboard#index'
+  
+  devise_for :users
+
+  resources :sales do
+    resources :sale_details
+  end
+
+  resources :items
+  resources :units
+  resources :towns
+  resources :categories
+  resources :brands
+
+  #get 'validate_suggested_brand/index'
+  get '/brands_suggestion', to: 'brands_suggestion#index'
+  get '/validate_suggested_brand', to: 'validate_suggested_brand#index'
+
+
+  #get 'validate_suggested_item/index'
+  get '/items_suggestion', to: 'items_suggestion#index'
+  get '/validate_suggested_item', to: 'validate_suggested_item#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
